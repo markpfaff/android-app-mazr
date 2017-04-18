afe11954f8a2ff6277d3a99d8ce56a12fa295962
markpfaff <markdpfaff@gmail.com>
2017-04-17 21:16:41 -0700

    test commit

---
+++ app/src/main/java/com/markpfaff/mazr/MainActivity.java
@@ -0,0 +1,42 @@
+package com.markpfaff.mazr;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            Log.d(TAG, "onCreate() Restoring previous state");
+            /* restore state */
+        } else {
+            Log.d(TAG, "onCreate() No saved state available");
+            /* initialize app */
+        }
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        if (view != null) {
+            Log.d(TAG, "view passed to sendMessage successfully");
+
+        } else {
+            Log.d(TAG, "no view to pass");
+
+        }
+    }
+}
